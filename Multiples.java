d18ab9efa1a2556b3de9648e5acff5975875f857
naasirmi <naasirmi@gmail.com>
2017-12-14 14:04:54 -0800

    Update Multiples.java

--- Multiples.java
+++ Multiples.java
@@ -1,11 +1,11 @@
-package com.codingiseasy;
+The0package com.codingiseasy;
 /*The list of all natural numbers below 10 that are multiples of 3 or 5, are: 3, 5, 6 and 9.
 The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.*/
 public class Multiples {
 static int sum=0;
 public static void function(int n){
-	for(int i=1; i<10; i++) {
+	for(int i=1; i<n; i++) {
 		if(i%3==0 || i%5==0) {
 		
 			sum=sum+i;
@@ -17,10 +17,10 @@ System.out.println(sum);
 }
 	public static void main(String[] args) {
 		
-		function(100);
+		function(1000);
 		
 
 	}
 }
 
-
+The answer is: 233168
